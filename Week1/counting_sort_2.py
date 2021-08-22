e593baa325e3981f6e81beba2d2ce76e04b6ddb3
Mintaa911 <61611671+Mintaa911@users.noreply.github.com>
2021-08-23 01:23:27 +0300

    Create counting_sort_2.py

---
+++ Week1/counting_sort_2.py
@@ -0,0 +1,9 @@
+def countingSort(arr):
+    countingArr = [0 for i in range(max(arr)+1)]
+    for i in arr:
+        countingArr[i] += 1
+    for j in range(len(countingArr)):
+        for x in range(countingArr[j]):
+            arr.append(j)
+            arr.pop(0)
+    return arr 
