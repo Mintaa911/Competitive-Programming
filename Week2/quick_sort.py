9813ebc9a4ee3043d491fa1e28d7a72a77a00269
Mintaa911 <61611671+Mintaa911@users.noreply.github.com>
2021-08-26 13:14:45 +0300

    Create quick_sort.py

---
+++ Week2/quick_sort.py
@@ -0,0 +1,17 @@
+def quick_sort(arr,l,h):
+    if l >= h:
+        return 
+    p = partition(arr,l,h)
+    quick_sort(arr,l,p-1)
+    quick_sort(arr,p+1,h)
+    return arr
+
+def partition(arr,l,h):
+    pivot = arr[h]
+    j = l -1
+    for i in range(l,h):
+        if arr[i] < pivot:
+            j += 1
+            arr[i],arr[j] = arr[j],arr[i]       
+    arr[h],arr[j+1] = arr[j+1],arr[h]
+    return j + 1
