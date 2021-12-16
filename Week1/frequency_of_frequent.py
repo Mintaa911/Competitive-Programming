a40961afb075fac3d4522a02bd709cf4dea25380
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-12-16 16:17:11 +0300

    most frequent

---
+++ Week1/frequency_of_frequent.py
@@ -0,0 +1,16 @@
+def frequent(nums,k):
+    nums.sort(reverse = True)
+    freq = 0
+    l, r = 0, 0
+    while r < len(nums):
+        k -= nums[l] - nums[r]
+        while k < 0:
+            k += (r - l) * (nums[l] - nums[l + 1])
+            l += 1
+            
+        r += 1
+        freq = max(freq, r - l)
+    return freq
+
+            
+            
