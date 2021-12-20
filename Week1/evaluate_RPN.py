427e4b329dba1242e230d4f6e192bf48f7b2affa
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-12-20 08:26:15 +0300

    evaluate RPN

---
+++ Week1/evaluate_RPN.py
@@ -0,0 +1,22 @@
+class Solution:
+    def evalRPN(tokens):
+        operations = {"+":"+","-":"-","/":"/","*":"*"}
+        stack = []
+
+        for token in tokens:
+            if token.startswith('-') and token[1:].isdigit():
+                stack.append(token)
+            elif token.isnumeric():
+                stack.append(token)
+            else: 
+
+                a = stack.pop()
+                b = stack.pop()
+                result = eval(f'{b} {operations[token]} ({a})')
+                if token == '/' and result > 0:
+                    result = math.floor(result)
+                else:
+                    result =  math.ceil(result)
+                    
+                stack.append(result)
+        return stack.pop()
