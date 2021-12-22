e26db0c557db6c526b114280b136154f4774c1cb
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-12-22 19:10:23 +0300

    next greater

---
+++ Week2/next_greater.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        greatDict = dict()
+        r = []
+        
+        for num in nums2:
+            if len(stack) == 0:
+                stack.append(num)
+            elif num > stack[-1]:
+                while len(stack) > 0 and num > stack[-1]:
+                    greatDict[stack.pop()] = num
+                stack.append(num)
+            else:
+                stack.append(num)
+            
+        for num in nums1:
+            if num in greatDict:
+                r.append(greatDict[num])
+            else:
+                r.append(-1)
+        return r
