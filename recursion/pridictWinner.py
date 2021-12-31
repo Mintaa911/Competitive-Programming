7cb8fcd6b313af4f04d7f2c0cc7e6b08aa15d402
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-12-31 14:42:04 +0300

    predict the winner player

---
+++ recursion/pridictWinner.py
@@ -0,0 +1,42 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        p1,p2 = self.divideConquer(nums,0,len(nums)-1,1)
+        
+        return p1 >= p2
+        
+    def divideConquer(self,nums,s,e,p):
+        p1,p2 = 0,0
+        if(s == e):
+            if p == 1:
+                return nums[s],0
+            else:
+                return 0,nums[s]
+        
+        if p == 1:
+            p1 = nums[s]
+            tp1,tp2 = self.divideConquer(nums,s+1,e,2)
+            curr1 = [p1+tp1,tp2]
+            
+            p1 = nums[e]
+            tp1,tp2= self.divideConquer(nums,s,e-1,2)
+            curr2 = [p1+tp1,tp2]
+
+            if curr1[0] > curr2[0]:
+                return curr1[0],curr1[1]
+            else:
+                return curr2[0],curr2[1]            
+        else:
+            p2 = nums[s]
+            tp1,tp2 = self.divideConquer(nums,s+1,e,1)
+            curr1 = [tp1,p2+tp2]
+
+            p2 = nums[e]
+            tp1,tp2= self.divideConquer(nums,s,e-1,1)
+            curr2 = [tp1,p2+tp2]
+
+            if curr1[1] > curr2[1]:
+                return curr1[0],curr1[1]
+            else:
+                return curr2[0],curr2[1]
+
+        
