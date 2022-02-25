d21fdd679ba0cf928fb1093a201eec06380e6781
Mintesnot Bezabhi <61611671+Mintaa911@users.noreply.github.com>
2022-02-25 09:41:34 +0300

    Update furthest_building_reached.py

--- bootcamp1/furthest_building_reached.py
+++ bootcamp1/furthest_building_reached.py
@@ -16,3 +16,46 @@ class Solution:
             
             
         return i+1
+               
+        
+        
+#         if len(heights) == 1:
+#             return 0
+#         min_ladder, max_brick = [], []
+#         previous = 1000001
+        
+#         idx = 0
+        
+#         while idx < len(heights):
+#             gap = heights[idx] - previous
+            
+#             if gap <= 0 :
+#                 previous = heights[idx]
+                
+#             elif gap > 0 and gap <= bricks:
+#                 bricks -= gap
+#                 heappush(max_brick,-gap)
+#                 previous = heights[idx] 
+#             elif gap > 0 and ladders > 0:
+#                 ladders -= 1
+#                 heappush(min_ladder,gap)
+#                 previous = heights[idx]                       
+                
+#             elif gap > 0 and max_brick and min_ladder:
+#                 if ((-max_brick[0]) - min_ladder[0] >= gap):
+#                     bricks += (-max_brick[0]) - min_ladder[0]
+#                     remove_l = heappop(min_ladder)
+#                     remove_b = -heappop(max_brick)
+#                     heappush(min_ladder,remove_b)
+#                     heappush(max_brick,remove_l)
+#                     bricks -= gap
+#                     previous = heights[idx]
+#                 else:
+#                     break
+#             else:
+#                 break
+                
+#             idx += 1
+            
+        # return idx -1
+                
