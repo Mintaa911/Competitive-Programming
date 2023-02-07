f3d6a2fe9358ab78359dd2cd4da1b97953d0fe1b
Mintesnot Bezabhi <61611671+Mintaa911@users.noreply.github.com>
2023-02-08 00:37:15 +0300

    Update Text Justification.py

--- squid_game/Text Justification.py
+++ squid_game/Text Justification.py
@@ -1,47 +1,43 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        ans, cur = [], []
-        chars = 0
-        
-        for word in words:
-            # if cur is empty or the total chars + total needed spaces + next word fit
-            if not cur or (len(word) + chars + len(cur)) <= maxWidth:
-                cur.append(word)
-                chars += len(word)
-            else:
-                # place spaces, append the line to the ans, and move on
-                line = self.placeSpacesBetween(cur, maxWidth - chars)
-                ans.append(line)
-                cur.clear()
-                cur.append(word)
-                chars = len(word)
-        
-        # left justify any remaining text, which is easy
-        if cur:
-            extra_spaces = maxWidth - chars - len(cur) + 1
-            ans.append(' '.join(cur) + ' ' * extra_spaces)
-            
-        return ans
-    
-    
-    def placeSpacesBetween(self, words, spaces):
-        if len(words) == 1: return words[0] + ' ' * spaces
-        
-        space_groups = len(words)-1
-        spaces_between_words = spaces // space_groups
-        extra_spaces = spaces % space_groups
-        
-        cur = []
-        for word in words:
-            cur.append(word)
-            
-            # place the min of remaining spaces or spaces between words plus an extra if available
-            cur_extra = min(1, extra_spaces)
-            spaces_to_place = min(spaces_between_words + cur_extra, spaces)
+        strt = 0
+        n = len(words)
+        prefix = [0]
+        group = []
+        ans = []
+        for i in range(n):
+            prefix.append(len(words[i]) + prefix[-1])
 
-            cur.append(' ' * spaces_to_place)
-            
-            if extra_spaces: extra_spaces -= 1
-            spaces -= spaces_to_place
+        for end in range(1,n+1):
+            if (prefix[end] - prefix[strt] + (end-strt-1)) > maxWidth:
+                group.append(words[strt:end-1])
+                strt = end-1
+        if strt <= n:
+            group.append(words[strt:])
         
-        return ''.join(cur)
+        for j in range(len(group)):
+            g = group[j]
+            l = len("".join(g))
+            even_gap = (maxWidth-l)//(len(g)-1) if len(g) > 1 else (maxWidth-l)
+            rm = (maxWidth-l)%(len(g)-1) if len(g) > 1 else (maxWidth-l)
+            if j == len(group) -1:
+                even_gap = 1
+                rm = 0
+            lst = []
+            for i in range(len(g)):
+                lst.append(g[i])
+                if len(g) == 1:
+                    lst.append(str(rm*" "))
+                elif rm > 0:
+                    lst.append(str(" "))
+                    rm -= 1
+                if i < (len(g)-1):
+                    lst.append(str(even_gap*" "))
+            if j == len(group) - 1:
+                l = maxWidth - len("".join(lst))
+                lst.append(str(l*" "))
+
+            ans.append("".join(lst))
+        
+        
+        return ans 
